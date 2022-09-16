cbae81ddf55557f29a9d0f23df6cbef7560242ed
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2022-09-16 09:20:17 +0700

    fungsi promise

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (sikap) => {
+
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+
+  const allTheater = [...theaterIXX, ...theaterVGC]
+
+  let countSikap = 0;
+
+  allTheater.forEach((film) => {
+    if (film.hasil === sikap) {
+      countSikap++;
+    }
+  })
+
+  return countSikap;
+
+};
 
 module.exports = {
   promiseOutput,
